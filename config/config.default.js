ee61bbaf216569fe9aaf16b6ccf356c531287249
宋佳豪 <79705935+Jacksong918@users.noreply.github.com>
2023-07-03 10:01:11 +0800

    Update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -41,15 +41,15 @@ module.exports = appInfo => {
   };
 
   config.userData = {
-    mineBirth: "1994-03-24", // 自己的生日
-    gfBirth: "1994-12-26", // 女朋友的生日
-    loveDay: "2007-08-11", // 在一起的日期
-    weatherCity: '茂名市' // 需要获取天气的城市，必须时xx市，xx县，xx自治区, 详细可以去utils/amap.js搜索到就可以，比如广州市，不能是广州
+    mineBirth: "2000-09-18", // 自己的生日
+    gfBirth: "2001-08-12", // 女朋友的生日
+    loveDay: "2023-05-13", // 在一起的日期
+    weatherCity: '昆山市' // 需要获取天气的城市，必须时xx市，xx县，xx自治区, 详细可以去utils/amap.js搜索到就可以，比如广州市，不能是广州
   }
 
   config.userCity = {
-    "adname":"茂名市",
-    "adcode":"440900"
+    "adname":"昆山市",
+    "adcode":"320583"
   }
 
   // 寄言 一旦设置了就不会请求接口，在这里随机返送一条
